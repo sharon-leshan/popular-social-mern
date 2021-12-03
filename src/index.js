2fabd94cb42b9c964860548942be05e75e257ec7
sharon-leshan <maleos380@gmail.com>
2021-12-03 22:15:05 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { StateProvider } from "./StateProvider";
 import reducer, { initialState } from "./Reducer";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>

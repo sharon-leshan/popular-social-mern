8e59af33360fffabad8cf554cde398daf0bc44ed
sharon-leshan <maleos380@gmail.com>
2021-12-03 11:52:23 +0300

    updated

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import SidebarRow from './SidebarRow'
-import LocalHospitalICon from '@material-ui/icons/LocalHospital'
-import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags'
-import PeopleIcon from '@material-ui/icons/People'
-import ChatIcon from '@material-ui/icons/Chat'
-import StorefrontIcon from '@material-ui/icons/Storefront'
-import VideoLibraryIcon from '@material-ui/icons/VideoLibrary'
-import ExpandMoreOutlined from '@material-ui/icons/ExpandMoreOutlined'
-import styled from 'styled-components'
-import {useStateValue} from '../StateProvider'
+import React from "react";
+import SidebarRow from "./SidebarRow";
+import LocalHospitalICon from "@material-ui/icons/LocalHospital";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import ExpandMoreOutlined from "@material-ui/icons/ExpandMoreOutlined";
+import styled from "styled-components";
+import { useStateValue } from "../StateProvider";
 
 const Sidebar = () => {
-    const [{user}, dispatch] = useStateValue()
-    return(
-        <SidebarWrapper>
-            <SidebarRow  src={user.photoURL} title={user.displayName}/>
-            <SidebarRow Icon={EmojiFlagsIcon} title="Pages"/>
-            <SidebarRow Icon={PeopleIcon} title="Friends"/>
-            <SidebarRow Icon={ChatIcon} title="Messenger"/>
-            <SidebarRow Icon={StorefrontIcon} title="Marketplace"/>
-            <SidebarRow Icon={VideoLibraryIcon} title="Video"/>
-            <SidebarRow Icon={ExpandMoreOutlined} title="More"/>
-
-        </SidebarWrapper>
-    )
-}
-const SidebarWrapper = styled.div``
-export default Sidebar
+  const [{ user }, dispatch] = useStateValue();
+  return (
+    <SidebarWrapper>
+      <SidebarRow src={user.photoURL} title={user.displayName} />
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messenger" />
+      <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Video" />
+      <SidebarRow Icon={ExpandMoreOutlined} title="More" />
+    </SidebarWrapper>
+  );
+};
+const SidebarWrapper = styled.div``;
+export default Sidebar;

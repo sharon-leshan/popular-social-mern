9da984bd2e1a3b29f93f7788a52f92ed3b280677
sharon-leshan <maleos380@gmail.com>
2021-12-04 15:00:09 +0300

    update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SidebarRow from "./SidebarRow";
-import LocalHospitalICon from "@material-ui/icons/LocalHospital";
-import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
-import PeopleIcon from "@material-ui/icons/People";
-import ChatIcon from "@material-ui/icons/Chat";
-import StorefrontIcon from "@material-ui/icons/Storefront";
-import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
-import ExpandMoreOutlined from "@material-ui/icons/ExpandMoreOutlined";
+import {
+  LocalHospital,
+  EmojiFlags,
+  People,
+  Chat,
+  Storefront,
+  VideoLibrary,
+  ExpandMoreOutlined,
+} from "@material-ui/icons/";
 import styled from "styled-components";
 import { useStateValue } from "../StateProvider";
 
@@ -15,11 +17,12 @@ const Sidebar = () => {
   return (
     <SidebarWrapper>
       <SidebarRow src={user.photoURL} title={user.displayName} />
-      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
-      <SidebarRow Icon={PeopleIcon} title="Friends" />
-      <SidebarRow Icon={ChatIcon} title="Messenger" />
-      <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
-      <SidebarRow Icon={VideoLibraryIcon} title="Video" />
+      <SidebarRow Icon={LocalHospital} title="COVID-19 Center" />
+      <SidebarRow Icon={EmojiFlags} title="Pages" />
+      <SidebarRow Icon={People} title="Friends" />
+      <SidebarRow Icon={Chat} title="Messenger" />
+      <SidebarRow Icon={Storefront} title="Marketplace" />
+      <SidebarRow Icon={VideoLibrary} title="Video" />
       <SidebarRow Icon={ExpandMoreOutlined} title="More" />
     </SidebarWrapper>
   );
